0b94d2d1d718e8fc918205b47327929f59150ac2
Vishal Balasubramanian <vishal.bala.nps@gmail.com>
2020-04-15 16:47:52 +0530

    Added dialogflow_handler

--- bot.py
+++ bot.py
@@ -1,10 +1,18 @@
 from flask import *
-
+import dialogflow_handler
 app = Flask(__name__)
 
-@app.route("/")
+#Routes
+@app.route("/",methods=["POST"])
 def handler():
-    return "Hello World!"
+    fres = {}
+    ihandler = dialogflow_handler.intent_handler(request.get_json())
+    intent = ihandler.get_intent()
+    params = ihandler.get_params()
+    if intent == "welcome_intent":
+        rhandler = dialogflow_handler.response_handler("Hello World!")
+        fres = rhandler.formResponse()
+    return jsonify(fres)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=8080)
---
+++ dialogflow_handler.py
@@ -0,0 +1,13 @@
+class intent_handler():
+    def __init__(self,dialogresjson):
+        self.resjson = dialogresjson
+    def get_intent(self):
+        return self.resjson["queryResult"]["intent"]["displayName"]
+    def get_params(self):
+        return self.resjson["queryResult"]["parameters"]
+
+class response_handler():
+    def __init__(self,text):
+        self.ftext = text
+    def formResponse(self):
+        return {"fulfillmentText":self.ftext}
