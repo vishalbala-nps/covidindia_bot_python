87152abfec19cf881ade149b7dcfc3a2ffea3869
Vishal Balasubramanian <vishal.bala.nps@gmail.com>
2020-04-16 13:18:45 +0530

    Added Reponses for get_statewise intent

--- bot.py
+++ bot.py
@@ -6,19 +6,29 @@ def on_launch():
     rhandler = dialogflow_handler.response_handler()
     rhandler.genericResponse("Hello! Welcome to Covidstate India! What can I do for you?")
     return rhandler.formResponse()
+
 def on_fallback():
     rhandler = dialogflow_handler.response_handler()
     rhandler.genericResponse("Sorry, I did not get that! Could you repeat it?")
     return rhandler.formResponse()
+
 def get_nationwide():
     data = requests.get("http://covidstate.in/api/v1/data?type=latest&state=India").json()
     rhandler = dialogflow_handler.response_handler()
     rhandler.genericResponse("As of "+data["timestamp"]["updated_time"]+", there are "+str(data["data"]["total"])+" infected people, "+str(data["data"]["deaths"])+" deaths and "+str(data["data"]["cured"])+" cured people. What else?")
     return rhandler.formResponse()
+
 def get_statewise(p):
     rhandler = dialogflow_handler.response_handler()
-    rhandler.genericResponse("The state is "+p["geo-state"])
-    return rhandler.formResponse()
+    if p["geo-state"] == None:
+        rhandler.genericResponse("Sorry, I did not get that! Could you repeat it?")
+        return rhandler.formResponse()
+    data = requests.get("http://covidstate.in/api/v1/data?state="+p["geo-state"]+"&type=latest")
+    if data.status_code == 200:
+        return rhandler.genericResponse("Got data").formResponse()
+    else:
+        return rhandler.genericResponse("Sorry, I could not find that state! Could you please repeat it?").formResponse()
+
 #Program Starts here
 app = Flask(__name__)
 
